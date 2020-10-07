47bc4665b8555cffbacb9ed5ff9d296e8c475450
Cédric Eberhardt <hello+code@cedeber.fr>
2020-10-08 00:19:41 +0200

    Guessing Game

--- guessing_game/src/main/kotlin/main.kt
+++ guessing_game/src/main/kotlin/main.kt
@@ -1,3 +1,22 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.random.Random
+
+fun main() {
+    println("Guess the number!")
+
+    val secretNumber = Random.nextInt(1, 100)
+    var guess: Int = -1
+
+    while (true) {
+        println("Please input your guess.")
+        guess = readLine()?.toIntOrNull() ?: continue
+        println("You guessed $guess")
+
+        when (guess.compareTo(secretNumber)) {
+            -1 -> println("Too small!")
+            1 -> println("Too big!")
+            else -> {
+                println("You win!"); break
+            }
+        }
+    }
 }
